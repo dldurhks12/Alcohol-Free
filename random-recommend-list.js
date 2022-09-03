86c029cd9e6897fa99e5e144bc67df35c13cb843
yundoryeong <dka2899@naver.com>
2022-09-03 21:48:43 +0900

    first commit

---
+++ random-recommend-list.js
@@ -0,0 +1,13 @@
+function click1(num) {
+  //사용하고자 하는 함수
+  alert(`click ${num}`);
+}
+function click2() {
+  //사용하고자 하는 함수
+  alert("click 2 ");
+}
+
+function click3() {
+  //사용하고자 하는 함수
+  alert("click 3 ");
+}
