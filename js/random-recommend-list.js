1b1c601fb572f69812d7c7854e88fb1a2f9e8912
dldurhks12 <rio544749@gmail.com>
2022-09-19 22:06:07 +0900

    commit

--- js/random-recommend-list.js
+++ js/random-recommend-list.js
@@ -11,3 +11,17 @@ function click3() {
   //사용하고자 하는 함수
   alert("click 3 ");
 }
+
+document.getElementById("random-btn").addEventListener("click", () => {
+  const row = document.querySelector(".row");
+  const load = document.querySelector(".loading");
+  row.classList.add("d-none");
+  load.classList.remove("d-none");
+
+  // TODO: 조건에 맞는 랜덤인지 확인하게
+  setTimeout(() => {
+    row.classList.remove("d-none");
+
+    location.href = ""; // 넘어갈 랜덤
+  }, 3000);
+});
